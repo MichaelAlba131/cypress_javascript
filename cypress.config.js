87254d1661f67667ebff960eb7669d642b539701
Michael Alba Pereira <michael.alba@outlook.com.br>
2025-05-31 22:34:25 +0100

    Set up Cypress project with Cucumber integration, Babel configuration, and Allure reporting

---
+++ cypress.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require('cypress');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+
+module.exports = defineConfig({
+  projectId: 'uqbvjv',
+  reporter: 'spec',
+  e2e: {
+    experimentalModifyObstructiveThirdPartyCode: true,
+    specPattern: 'cypress/e2e/step_definitions/**/*.feature',
+    async setupNodeEvents(on, config) {
+      config.env = config.env || {};
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on('file:preprocessor', createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }));
+
+      allureWriter(on, config);
+
+      return config;
+    },
+  },
+  env: {
+    'cypress-cucumber-preprocessor': {
+      stepDefinitions: 'cypress/e2e/step_definitions/**/*.js'
+    }
+  }
+});
